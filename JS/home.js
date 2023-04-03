6225eeb504c353ec817f21cfa90d36988e9c8c6b
Luiz A. Tavares <luizalvarengatavares@gmail.com>
2023-04-03 09:08:45 -0300

    primerio comit 03.04.23

---
+++ JS/home.js
@@ -0,0 +1,248 @@
+//VERIFICAÇÃO DE LOGIN
+let usuarioLogado = JSON.parse(sessionStorage.getItem("UsuarioLogado" ?? "null"));
+if (!usuarioLogado) {
+    alert("Você precisa estar logado para utilizar essa aplicação!");
+    window.location.href = "index.html";
+};
+
+//CARREGAR DADOS DO USUARIO
+document.getElementById('tituloHome').innerHTML = `Olá ${usuarioLogado.nomeUsuario}!`;
+document.addEventListener('DOMContentLoaded', () => {
+    const recadosUsuarioLogado = pegarRecadosUsuario();
+    if (recadosUsuarioLogado.length > 0) {
+        document.getElementById('exRecado1').remove();
+        document.getElementById('exRecado2').remove();
+        carregarDadosUsuario();
+    }
+})
+
+function carregarDadosUsuario() {
+    const recadosUsuarioLogado = pegarRecadosUsuario();
+    for (recado of recadosUsuarioLogado) {
+        criarRecadoHTML(recado);
+    }
+}
+
+//CRIAR NOVO RECADO
+// const modalCriarRecado = new bootstrap.Modal(document.getElementById('modalCriarRecado'), {});
+const btnCriarRecado = document.getElementById('btnCriarRecado');
+btnCriarRecado.addEventListener('click', confereCriarRecado);
+
+const labelIdRecado = document.getElementById('labelIdRecado');
+const idRecado = document.getElementById('idRecado');
+idRecado.addEventListener('mouseenter', () => {
+    labelIdRecado.innerHTML = 'Identificador:';
+    labelIdRecado.removeAttribute('style');
+    idRecado.removeAttribute('style');
+})
+
+const labelTituloRecado = document.getElementById('labelTituloRecado');
+const tituloRecado = document.getElementById('tituloRecado');
+tituloRecado.addEventListener('mouseenter', () => {
+    labelTituloRecado.innerHTML = 'Escolha um título:';
+    labelTituloRecado.removeAttribute('style');
+    tituloRecado.removeAttribute('style');
+})
+
+const labelDescricaoRecado = document.getElementById('labelDescricaoRecado');
+const descricaoRecado = document.getElementById('descricaoRecado');
+descricaoRecado.addEventListener('mouseenter', () => {
+    labelDescricaoRecado.innerHTML = 'Descrição do recado:';
+    labelDescricaoRecado.removeAttribute('style');
+    descricaoRecado.removeAttribute('style');
+})
+
+function confereCriarRecado(e) {
+    e.preventDefault()
+    if (idRecado.value === "") {
+        idRecado.setAttribute('style', 'background-color: #ff9999');
+        labelIdRecado.innerHTML = "Você deve adicionar um identificador!";
+        labelIdRecado.setAttribute('style', 'color: #ff9999');
+        return
+    }
+
+    if (tituloRecado.value === "") {
+        tituloRecado.setAttribute('style', 'background-color: #ff9999');
+        labelTituloRecado.innerHTML = "Você deve adicionar um titulo!";
+        labelTituloRecado.setAttribute('style', 'color: #ff9999');
+        return
+    }
+
+    if (descricaoRecado.value === "") {
+        descricaoRecado.setAttribute('style', 'background-color: #ff9999');
+        labelDescricaoRecado.innerHTML = "Você deve adicionar uma descrição!";
+        labelDescricaoRecado.setAttribute('style', 'color: #ff9999');
+        return
+    }
+    const novoRecado = {
+        id: idRecado.value,
+        titulo: tituloRecado.value,
+        descricao: descricaoRecado.value
+    }
+
+
+    const recadosUsuario = pegarRecadosUsuario();
+    const existeId = recadosUsuario.some((recado) => recado.id === idRecado.value);
+    if (existeId) {
+        alert('Já existe um recado com esse ID, por favor tente com outro número.')
+        return
+    }
+    recadosUsuario.push(novoRecado);
+    salvarRecadosUsuario(recadosUsuario);
+    criarRecadoHTML(novoRecado);
+}
+
+function criarRecadoHTML(recado) {
+    const espacoRecados = document.getElementById('espacoRecados');
+
+    const novoRecado = document.createElement('div');
+    novoRecado.setAttribute('id', `recado${recado.id}`);
+    novoRecado.setAttribute('class', 'col-sm-6 mb-3 mb-sm-0 ');
+
+    const novoCard = document.createElement('div');
+    novoCard.setAttribute('class', 'card m-2');
+
+    const novoCardBody = document.createElement('div');
+    novoCardBody.setAttribute('class', 'card-body');
+
+    const novoTituloRecado = document.createElement('h5');
+    novoTituloRecado.setAttribute('class', 'tituloRecado card-title');
+    novoTituloRecado.innerHTML = `${recado.titulo}`;
+
+    const novaDescricao = document.createElement('p');
+    novaDescricao.setAttribute('class', 'card-text');
+    novaDescricao.innerHTML = `${recado.descricao}`;
+
+    const rowDosBotoes = document.createElement('div');
+    rowDosBotoes.setAttribute('class', 'row');
+    const colDosBotoes = document.createElement('div');
+    colDosBotoes.setAttribute('class', 'col-12 justify-content-between');
+
+    const btnApagar = document.createElement('button');
+    btnApagar.setAttribute('class', 'btn btn-danger me-1');
+    btnApagar.setAttribute('type', 'button');
+    btnApagar.setAttribute('data-bs-target', '#modalApagar');
+    btnApagar.setAttribute('data-bs-toggle', 'modal');
+    btnApagar.addEventListener('click', (e) => {
+        e.preventDefault();
+        apagarRecado(recado);
+    });
+    btnApagar.innerHTML = "APAGAR";
+
+    const btnEditar = document.createElement('button');
+    btnEditar.setAttribute('class', 'btn btn-success');
+    btnEditar.setAttribute('type', 'button');
+    btnEditar.setAttribute('data-bs-toggle', 'modal');
+    btnEditar.setAttribute('data-bs-target', '#modalEditarRecado');
+    btnEditar.addEventListener('click', (e) => {
+        e.preventDefault();
+        editarRecado(recado);
+    });
+    btnEditar.innerHTML = "EDITAR";
+
+    espacoRecados.appendChild(novoRecado);
+    novoRecado.appendChild(novoCard);
+    novoCard.appendChild(novoCardBody);
+    novoCardBody.appendChild(novoTituloRecado);
+    novoCardBody.appendChild(novaDescricao);
+    novoCardBody.appendChild(rowDosBotoes);
+    rowDosBotoes.appendChild(colDosBotoes);
+    colDosBotoes.appendChild(btnApagar);
+    colDosBotoes.appendChild(btnEditar);
+
+    // modalCriarRecado.hide();
+}
+
+//EDITAR RECADO
+const idEdit = document.getElementById('idEditarRecado');
+const tituloEdit = document.getElementById('tituloEditarRecado');
+const descricaoEdit = document.getElementById('descricaoEditarRecado');
+
+function editarRecado(recado) {
+    idEdit.value = recado.id;
+    tituloEdit.value = recado.titulo;
+    descricaoEdit.value = recado.titulo;
+
+    document.getElementById('btnEditarRecado').addEventListener('click', (e) => {
+        e.preventDefault();
+        salvarRecadoEdit(idEdit.value)
+    })
+}
+
+function salvarRecadoEdit(idEdit) {
+    console.log("entrou no salvarEdit")
+    let recadosUsuario = pegarRecadosUsuario();
+    const indexEdit = recadosUsuario.findIndex((recado) => {
+        return recado.id === idEdit
+    })
+
+    if (indexEdit >= 0) {
+        const recadoEdit = {
+            id: idEdit,
+            novoTitulo: tituloEdit.value,
+            novaDescricao: descricaoEdit.value
+        }
+
+        recadosUsuario[indexEdit].titulo === recadoEdit.novoTitulo;
+        recadosUsuario[indexEdit].descricao === recadoEdit.novaDescricao;
+        console.log(recadoEdit)
+    }
+
+    console.log(recadosUsuario[indexEdit]);
+    console.log(recadosUsuario)
+
+
+    salvarRecadosUsuario(recadosUsuario)
+    // window.location.href = 'home.html'
+}
+
+//APAGAR RECADO
+function apagarRecado(recadoApagar) {
+    document.getElementById('textoApagar').innerHTML = `Tem certeza que deseja apagar o recado ${recadoApagar.id}? O mesmo será excluído permanentemente!`
+    document.getElementById('btnApagarRecado').addEventListener('click', () => {
+        const recadosUsuario = pegarRecadosUsuario();
+        const indexApagar = recadosUsuario.findIndex((recado) => {
+            return recado.id === recadoApagar.id
+        })
+        if (indexApagar >= 0) {
+            recadosUsuario.splice(indexApagar, 1)
+            alert("Recado apagado com sucesso");
+            salvarRecadosUsuario(recadosUsuario)
+            document.getElementById(`recado${recadoApagar.id}`).remove();
+        }
+    })
+}
+
+//FUNCOES GLOBAIS
+function buscarListaUsuarios() {
+    return JSON.parse(localStorage.getItem("usuarios" || []))
+}
+
+function salvarRecadosUsuario(listaRecados) {
+    const listaUsuarios = buscarListaUsuarios();
+    const indiceUser = indiceUsuarioLogado();
+    listaUsuarios[indiceUser].recadosUsuario = listaRecados;
+    localStorage.setItem('usuarios', JSON.stringify(listaUsuarios))
+}
+
+function pegarRecadosUsuario() {
+    const listaUsuarios = buscarListaUsuarios();
+    const indiceUser = indiceUsuarioLogado();
+    return listaUsuarios[indiceUser].recadosUsuario;
+}
+
+function indiceUsuarioLogado() {
+    const listaUsuarios = buscarListaUsuarios();
+    const indiceUser = listaUsuarios.findIndex((usuario) => {
+        return usuario.emailUsuario === usuarioLogado.emailUsuario
+    });
+
+    return indiceUser
+}
+
+//LOGOUT
+const btnLogOut = document.getElementById('btnLogOut');
+btnLogOut.addEventListener('click', () => {
+    sessionStorage.setItem("UsuarioLogado", "null");
+    window.location.href = "index.html"
+})
---
+++ JS/index.js
@@ -0,0 +1,148 @@
+// CHAMA MODAL CADASTRO
+const btnChamaCriarConta = document.getElementById('chamaCriarConta');
+const modalCadastro = document.getElementById('modalCadastro');
+
+btnChamaCriarConta.addEventListener('click', () => {
+    modalCadastro.classList.add('scale-up-center');
+})
+
+// CADASTRA USUARIO
+const btnCriarConta = document.getElementById('btnCriarConta');
+btnCriarConta.addEventListener('click', confereCriarConta);
+
+const nomeCadastro = document.getElementById('nomeCadastro');
+const labelNomeCadastro = document.getElementById('labelNomeCadastro');
+nomeCadastro.addEventListener('mouseenter', () => { labelNomeCadastro.removeAttribute('style') });
+
+const emailCadastro = document.getElementById('emailCadastro');
+const labelEmailCadastro = document.getElementById('labelEmailCadastro');
+emailCadastro.addEventListener('mouseenter', () => { labelEmailCadastro.removeAttribute('style') });
+
+const senhaCadastro = document.getElementById('senhaCadatro');
+const labelSenhaCadastro = document.getElementById('labelSenhaCadastro');
+senhaCadastro.addEventListener('mouseenter', () => {
+    labelSenhaCadastro.removeAttribute('style'),
+        labelSenhaCadastro.innerHTML = "Senha", senhaCadastro.removeAttribute('style')
+});
+
+const confirmaSenhaCadastro = document.getElementById('confirmaSenhaCadastro')
+const labelConfirmaSenhaCadastro = document.getElementById('labelConfirmaSenhaCadastro')
+confirmaSenhaCadastro.addEventListener('mouseenter', () => {
+    labelConfirmaSenhaCadastro.removeAttribute('style'),
+        labelConfirmaSenhaCadastro.innerHTML = "Confirme sua senha", confirmaSenhaCadastro.removeAttribute('style')
+});
+
+function confereCriarConta(e) {
+    e.preventDefault()
+    if (nomeCadastro.value === "") {
+        nomeCadastro.focus();
+        nomeCadastro.setAttribute('style', 'background-color: #ff9999');
+        labelNomeCadastro.setAttribute('style', 'color: #ff9999');
+        return
+    }
+
+    if (emailCadastro.value === "") {
+        emailCadastro.focus();
+        emailCadastro.setAttribute('style', 'background-color: #ff9999');
+        labelEmailCadastro.setAttribute('style', 'color: #ff9999');
+        return
+    }
+
+    if (senhaCadastro.value === "") {
+        senhaCadastro.focus();
+        senhaCadastro.setAttribute('style', 'background-color: #ff9999');
+        labelSenhaCadastro.setAttribute('style', 'color: #ff9999');
+        return
+    }
+
+    if (confirmaSenhaCadastro.value === "") {
+        confirmaSenhaCadastro.focus();
+        confirmaSenhaCadastro.setAttribute('style', 'background-color: #ff9999');
+        labelConfirmaSenhaCadastro.setAttribute('style', 'color: #ff9999');
+        return
+    }
+
+    if (senhaCadastro.value != confirmaSenhaCadastro.value) {
+        labelSenhaCadastro.setAttribute('style', 'color: #ff9999');
+        labelConfirmaSenhaCadastro.setAttribute('style', 'color: #ff9999');
+        labelSenhaCadastro.innerHTML = "As senhas não coincidem!";
+        labelConfirmaSenhaCadastro.innerHTML = "As senhas não coincidem!";
+    }
+
+    verificarConta();
+};
+
+function verificarConta() {
+    let usuarios = buscarListaUsuarios();
+    let existeConta = usuarios.some((metaEmail) => metaEmail.emailUsuario === emailCadastro.value);
+
+    if (existeConta) {
+        alert('Este e-mail já está cadastrado no sistema')
+        let emailLogin = document.getElementById("emailLogin")
+        emailLogin.value = emailCadastro.value
+        return
+    };
+
+    alert("Parabéns! Sua conta foi criada com sucesso.");
+    //modalCadastro.hide(); // PRECISO DESCOBRIR COMO ESCONDER O MODAL DEPOIS DE CRIAR A CONTA
+    criarContaNoStorage();
+};
+
+
+function criarContaNoStorage() {
+    const listaUsuarios = buscarListaUsuarios()
+
+    const nomeUsuario = nomeCadastro.value;
+    const emailUsuario = emailCadastro.value;
+    const senhaUsuario = senhaCadastro.value;
+    const recadosUsuario = [];
+    const usuario = {
+        nomeUsuario,
+        emailUsuario,
+        senhaUsuario,
+        recadosUsuario,
+
+    };
+
+    listaUsuarios.push(usuario);
+    atualizarUsuarios(listaUsuarios);
+};
+
+function atualizarUsuarios(listaUsuarios) {
+    return window.localStorage.setItem("usuarios", JSON.stringify(listaUsuarios));
+};
+
+
+// Fazer login
+const emailLogin = document.getElementById('emailLogin');
+const senhaLogin = document.getElementById('senhaLogin');
+const btnFormLogin = document.getElementById('btnFormLogin');
+btnFormLogin.addEventListener('click', confereLogin);
+
+function confereLogin() {
+    const usuaruios = buscarListaUsuarios();
+    const email = emailLogin.value;
+    const senha = senhaLogin.value;
+
+    const usuario = usuaruios.find((metaEmail) => metaEmail.emailUsuario === email);
+
+    if (!usuario) {
+        alert("Não foi possivel encontrar uma conta com o email fornecido!")
+        return
+    };
+
+    if (usuario.senhaUsuario != senha) {
+        alert("A senha está incorreta. Por favor, tente novamente.")
+        return
+    };
+
+    sessionStorage.setItem("UsuarioLogado", JSON.stringify(usuario));
+    window.location.href = "./home.html";
+}
+
+
+
+//FUNÇÕES GLOBAIS
+function buscarListaUsuarios() {
+    return JSON.parse(localStorage.getItem("usuarios")) ?? []
+};
